4cbdb79bfb005b6e7b3be874b43f87e968404ceb
Novikau Arseniy <novikov.arsenij@gmail.com>
2024-11-16 21:51:25 +0300

    create ServerMain & Handler

--- Client/src/main/java/test/ClientMain.java
+++ Client/src/main/java/test/ClientMain.java
@@ -1,8 +1,7 @@
 package test;
 
+
 public class ClientMain {
     public static void main(String[] args) {
-        Command test = new Command();
-        System.out.println("Hello World!");
     }
 }
--- Server/src/main/java/test/ServerMain.java
+++ Server/src/main/java/test/ServerMain.java
@@ -1,7 +1,76 @@
 package test;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ServerMain {
+    private static int port;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Dotenv dotenv = Dotenv.load();
+        port = Integer.parseInt(Objects.requireNonNull(dotenv.get("PORT")));
+
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            while(true) {
+                Socket socket = serverSocket.accept();
+
+                new Thread(new Handler(socket)).start();
+            }
+        }
+        catch (IOException e) {
+            System.err.println("Ошибка при работе сервера" + e);
+        }
+    }
+}
+
+class Handler implements Runnable {
+    Socket socket;
+
+    public Handler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (
+                InputStream input = socket.getInputStream();
+                OutputStream output = socket.getOutputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                PrintWriter writer = new PrintWriter(output, true)
+        ) {
+            writer.println("Добро пожаловать на сервер!");
+            String message;
+
+            while ((message = reader.readLine()) != null) {
+                System.out.println("Сообщение от клиента: " + message);
+
+                writer.println("Сервер получил: " + message);
+
+                if (message.equalsIgnoreCase("exit")) {
+                    writer.println("Прощайте!");
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Ошибка при общении с клиентом: " + e.getMessage());
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Ошибка при закрытии соединения: " + e.getMessage());
+            }
+        }
     }
 }
