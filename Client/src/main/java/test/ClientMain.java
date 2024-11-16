25019f6cb99e401dca25a441a30739ba3a5b6534
Novikau Arseniy <novikov.arsenij@gmail.com>
2024-11-16 21:16:03 +0300

    initial

---
+++ Client/src/main/java/test/ClientMain.java
@@ -0,0 +1,8 @@
+package test;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Command test = new Command();
+        System.out.println("Hello World!");
+    }
+}
