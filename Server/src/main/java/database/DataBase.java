223348381a609cf4e5fad947578769980d3eca74
Novikau Arseniy <novikov.arsenij@gmail.com>
2024-11-18 22:08:46 +0300

    add prepareStatement

--- Server/src/main/java/database/DataBase.java
+++ Server/src/main/java/database/DataBase.java
@@ -12,16 +12,19 @@ import static java.lang.System.*;
 
 public class DataBase {
     private static Connection con;
-    private static Statement st;
+
     public static void main(String[] args) throws SQLException {
         connectToDataBase();
         try {
             //add(new Authorization("Senya", "1111"));
-            Statement stmt = con.createStatement();
-            String login = "Senya";  // Значения получены от пользователя
-            String sql = "SELECT * FROM USERS WHERE NAME = '" + login+"'";
+            String login = "Misha";  // Значения получены от пользователя
+            String sql = "SELECT * FROM USERS WHERE NAME = ?";
 
-            ResultSet rs = stmt.executeQuery(sql);
+            PreparedStatement pstmt = con.prepareStatement(sql);
+
+            pstmt.setString(1, login);
+            ResultSet rs = pstmt.executeQuery();
+
 
             if (rs.next()) {
                 System.out.println("User found: " + rs.getString("name"));
@@ -55,15 +58,16 @@ public class DataBase {
     }
     public static void add(Authorization auth) throws SQLException {
 
-        st = con.createStatement();
+        Statement st = con.createStatement();
         String login = auth.getLogin(); // Замените на значение login
         String hash = Sha256.hash(auth.getPassword());
-        String req = "INSERT INTO USERS(NAME, HASHED_PASSWORD) VALUES (" + "'" + login+"'" + ",   '" + hash+"' )";// Замените на значение password
-        try {
-            st.execute(req);
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
+        String req = "INSERT INTO USERS(NAME, HASHED_PASSWORD) VALUES(?, ?);";// Замените на значение password
 
-        }
+        PreparedStatement preparedStatement = con.prepareStatement(req);
+
+        preparedStatement.setString(1, login); // Установка значения для первого параметра
+        preparedStatement.setString(2, hash); // Установка значения для второго параметра
+
+        preparedStatement.executeUpdate();
     }
 }
